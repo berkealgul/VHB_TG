d34cc7ed8db52e924596b0eb88f4d63162f313b5
berkealgul <berkealgul@gmail.com>
2022-11-04 09:16:04 +0300

    changes wip

--- hbg.py
+++ hbg.py
@@ -1,5 +1,6 @@
-import numpy as np
+import os
 import cv2
+import numpy as np
 
 
 class hbg:
@@ -21,13 +22,34 @@ class hbg:
         cv2.putText(frame, str(self.beat),(0, self.label_height), self.font, self.font_scale, (255, 255, 255), self.font_thickness, cv2.LINE_AA)  
         self.writer.write(frame)
 
-    def linear_rise(self, dt):
-        for i in range(1, dt*self.fps):
-            self.beat = self.beat + 1
+    # t is time in seconds
+    # final beat is desired end beat
+    # linear function 
+    def linear_change(self, t, final_beat):
+        frames = t*self.fps
+        for i in range(t*self.fps):
+            self.beat = self.beat + int((i * (final_beat - self.beat)) / frames)
             self.write_frame() 
+    
+    # x^2 function 
+    def rapid_change(self, t, final_beat):
+        # not finished
+        frames = t*self.fps
+        for i in range(t*self.fps):
+            self.beat = 0
+            self.write_frame() 
+
+    def constant_change(self, t):
+        for i in range(t*self.fps):
+            self.write_frame() 
+    
+    def sudden_change(self, new_beat):
+        self.beat = new_beat
+        self.write_frame()
 
     def stop(self):
         self.writer.release()
+        # command ffmpeg to create video frm sequence
 
 
 # fast debug lines
