cc8576ee1b59d9ba83b1134cc4a4440af6631a9e
berkealgul <berkealgul@gmail.com>
2022-12-03 01:59:23 +0300

    animation cycle goes well bruh

--- vhb.py
+++ vhb.py
@@ -3,15 +3,38 @@ import cv2
 import numpy as np
 
 
+
 class lsg:
-    def __init__(self, inner_color, outer_color, lung_dir, fps=25, output_dir="/output", video_name="output"):
+    def __init__(self, inner_color, outer_color, lung_dir, fps=25, output_dir="./output", video_name="output"):    
         self.fps = fps
         self.video_name = video_name
         self.output_dir = output_dir
         # BGR format
         self.inner_color = inner_color
         self.outer_color = outer_color
+        # constants
+        self.anim_len = 10 # len in frames
+        self.breating_size = 1.15
+        # funcs
         self.split_layers(lung_dir)
+
+    # animates 1 breating cycle
+    def animate_breating(self):
+        lung = self.inner_lung + self.outer_lung
+        w0, h0 = lung.shape[1], lung.shape[0]
+        for i in range(self.anim_len):
+            # dunno why but w and h increases at different scale
+            w = w0 + int(w0*(i/self.anim_len)*(self.breating_size-1))
+            h = h0 + int(h0*(i/self.anim_len)*(self.breating_size-1))
+            frame = cv2.resize(lung, (w,h), interpolation = cv2.INTER_AREA)
+            bl = int((w-w0)/2)
+            br = w - bl
+            bu = int((h-h0)/2)
+            bd = h - bu
+            # god know how but  frame remains at 1080x1000
+            self.write_frame(frame[bu:bd, bl:br, :])
+            print(frame[bu:bd, bl:br, :].shape)
+            print(w,h)
 
     def split_layers(self, lung_dir):
         lung = cv2.imread(lung_dir)
@@ -23,27 +46,20 @@ class lsg:
 
     def extract_color(self, img, color):
         # source image is rgb but seperated layers will be rgba
-        mask = np.zeros((img.shape[0], img.shape[1], 4))
+        mask = np.zeros(img.shape)
         for i in range(3):
             mask[:,:,i] = np.where(img[:,:,i] == color[i], color[i], 0)     
         return mask
 
     def start(self, initial_air=1.0):
+        self.check_dirs()
         self.time = 0
         self.air = min(1.0, initial_air) # range is [-1.0 1.0] below 0 represent water level
+        self.time = 0
+        self.n_frames = 0
 
     def set_air_level(self, new_air):
         self.air = new_air
-
-    def stop(self):
-        # ffmpeg assumes output is 25 fps. it is not a problem now but maybe it will need to be changed later
-        os.system("ffmpeg -i output/images/%d.png -vcodec qtrle "+self.output_dir+"/"+self.video_name+".mov")
-
-    def start(self, initial_air):
-        self.check_dirs()
-        self.set_air_level(initial_air)
-        self.time = 0
-        self.n_frames = 0
 
     def check_dirs(self):
         try:
@@ -51,10 +67,14 @@ class lsg:
         except OSError as error:
             print("output dirs already created")
 
-    def write_frame(self):
-        frame = np.zeros((self.label_height + self.baseline, self.label_width, 4), np.uint8)
-        cv2.imwrite(self.output_dir+"/images/"+str(self.n_frames)+".png", frame)
+    def write_frame(self, frame):
+        cv2.imwrite(self.output_dir+"/images_lung/"+str(self.n_frames)+".png", frame)
         self.n_frames = self.n_frames + 1
+    
+    def stop(self):
+        # ffmpeg assumes output is 25 fps. it is not a problem now but maybe it will need to be changed later
+        os.system("ffmpeg -i "+self.output_dir+"/images_lung/%d.png -vcodec qtrle "+self.output_dir+"/"+self.video_name+".mov")
+
 
 
 class hbtg:
@@ -119,7 +139,9 @@ def hbtg_test():
 
 def lsg_test():
     lsg_ = lsg((255,216,0), (249, 77, 4), "resources/full_lung_wb.png")
-
+    lsg_.start()
+    lsg_.animate_breating()
+    #lsg_.stop()
 
 if __name__ == "__main__":  
     lsg_test()
